497f7cb19a9f2cf62fd08dd9111b771dfff142c7
sanketgadhave <sanketmgadhave@gmail.com>
2023-12-09 11:21:13 -0500

    Updated app.py to add comments and also updated requirements file to update latest sklearn version

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
+# Import other necessary libraries and functions
 import numpy as np
 import streamlit as st
 from joblib import load
 import pandas as pd
 import time
 import matplotlib.pyplot as plt
-
-
-# Import other necessary libraries and functions
 
 # Load your model and any other required assets
 model_pred = load('Models/gradient_boosting_model.joblib')
@@ -15,17 +13,16 @@ levy_scaler = load('Models/levy_scaler.joblib')
 full_pipeline = load('Models/full_pipeline.joblib')
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-# Define a function to preprocess input data
 
+## Define a functions to preprocess input data
+
+# Function to preprocess Mileage and Levy column
 def st_transform(input_d):
-    # Replace ' km' in Mileage
     input_d["Mileage"] = input_d["Mileage"].astype(str).str.replace(' km', '').astype(int)
-
-    # Handle Levy
     input_d["Levy"] = input_d["Levy"].astype(str).str.replace("-", '').replace("", '0').astype(float)
     return input_d
 
-
+# Function to encode few features
 def cus_encoder(X):
     # Encoding logic
     X['Doors'].replace(['2', '4', "Other"], [1, 2, 3], inplace=True)
@@ -37,19 +34,20 @@ def cus_encoder(X):
 
     return X
 
-
+# Function to scale Mileage and Levy
 def transform_input(input_df_trans):
     input_df_trans['Mileage'] = mileage_scaler.transform(input_df_trans[['Mileage']])
     input_df_trans['Levy'] = levy_scaler.transform(input_df_trans[['Levy']])
     return input_df_trans
 
 
+# Function to perform Column Transform
 def transform_input_for_prediction(input_df_trans_pred):
-    # Assuming input_df is a single-row DataFrame with the same structure as the training data
     print(input_df_trans_pred.shape)
     transformed_data = full_pipeline.transform(input_df_trans_pred)
     return transformed_data
 
+#Preprocessin Function to call all the preprocessin taks defined
 def preprocess_data(input_data):
     initial_transformed_df_1 = st_transform(input_data)
     initial_transformed_df_2 = cus_encoder(initial_transformed_df_1)
@@ -65,8 +63,34 @@ def preprocess_data(input_data):
     return transformed_row
 
 
+# User defined functions for input field validation:
+def map_to_existing_category(user_input_category):
+    # Defining a mapping from user input "Other" to your existing categories
+    category_mapping = {
+        "Other": "Sedan",
+    }
 
-# Initialize session state for page navigation and user inputs
+    # Checking if the user input category is in the mapping
+    if user_input_category in category_mapping:
+        return category_mapping[user_input_category]
+    else:
+        return user_input_category
+
+def map_to_existing_fuel_type(user_input_fuel):
+    # Defining a mapping from user input "Other" to your existing categories
+    fuel_mapping = {
+        "Other": "Petrol",
+    }
+
+    # Checking if the user input category is in the mapping
+    if user_input_fuel in fuel_mapping:
+        return fuel_mapping[user_input_fuel]
+    else:
+        return user_input_fuel
+
+
+
+# Initializing session state for page navigation and user inputs
 if 'page_number' not in st.session_state:
     st.session_state.page_number = 1
 
@@ -78,35 +102,9 @@ if 'report_page' not in st.session_state:
 
 if 'prediction_made' not in st.session_state:
     st.session_state.prediction_made = False
+
+
 # Define pages
-def map_to_existing_category(user_input_category):
-    # Define a mapping from user input "Other" to your existing categories
-    category_mapping = {
-        "Other": "Sedan",  # Replace with the actual category name
-    }
-
-    # Check if the user input category is in the mapping
-    if user_input_category in category_mapping:
-        return category_mapping[user_input_category]
-    else:
-        return user_input_category
-
-def map_to_existing_fuel_type(user_input_fuel):
-    # Define a mapping from user input "Other" to your existing categories
-    fuel_mapping = {
-        "Other": "Petrol",  # Replace with the actual category name
-    }
-
-    # Check if the user input category is in the mapping
-    if user_input_fuel in fuel_mapping:
-        return fuel_mapping[user_input_fuel]
-    else:
-        return user_input_fuel
-
-
-
-
-
 def page1():
     st.header("Car Model Details")
     st.session_state.user_inputs['Levy'] = st.number_input('Levy', min_value=0, value=st.session_state.user_inputs.get('Levy', 0))
@@ -125,7 +123,6 @@ def page1():
         st.error("Please enter a Manufacturer.")
 
     st.session_state.user_inputs['Model'] = st.text_input('Model', value=st.session_state.user_inputs.get('Model', ''))
-    year_range = range(1900, 2024)
     st.session_state.user_inputs['Prod_year'] = st.slider('Production Year', min_value=1900, max_value=2023, value=st.session_state.user_inputs.get('Prod_year', 2023))
 
     with open('meta/category.txt', 'r') as file:
@@ -157,7 +154,6 @@ def page2():
     index = fuel_list.index(default_fuel_type)
     st.session_state.user_inputs['Fuel_type'] = st.selectbox('Fuel_type', fuel_list, index=index)
     st.session_state.user_inputs['Fuel_type'] = map_to_existing_fuel_type(st.session_state.user_inputs['Fuel_type'])
-    #st.session_state.user_inputs['Fuel_type'] = st.selectbox('Fuel Type', ['Petrol', 'Diesel', 'Electric', 'Hybrid', 'LPG', 'CNG'], index=0 if st.session_state.user_inputs.get('Fuel_type', 'Petrol') == 'Petrol' else 6)
     st.session_state.user_inputs['Engine_volume'] = st.text_input('Engine Volume', value=st.session_state.user_inputs.get( 'Engine_volume', ''))
     if 'Mileage' in st.session_state.user_inputs:
         mileage = st.session_state.user_inputs['Mileage']
@@ -202,6 +198,8 @@ def page3():
             st.session_state.warning_displayed = True
             st.stop()
 
+st.set_page_config(layout="wide")
+
 # Function to save inputs and move to next page
 def next_page():
     st.session_state.page_number += 1
@@ -210,10 +208,8 @@ def next_page():
 def prev_page():
     st.session_state.page_number -= 1
 
-st.set_page_config(layout="wide")
+# Function to go to report page and generate report
 def report_page():
-    # st.header("Prediction Report")
-    # st.markdown(f"### Estimated Price: ${st.session_state.estimated_price}")
     st.header("Prediction Report")
     st.markdown("### User Input Summary")
     user_input_items = list(st.session_state.user_inputs.items())
@@ -255,15 +251,13 @@ def report_page():
                     """, unsafe_allow_html=True)
     st.markdown(f"<h3 style='color:green;'>Estimated Price: ${st.session_state.estimated_price}</h3>",
                 unsafe_allow_html=True)
-    #st.markdown("## Factors affecting price")
     st.markdown("<h3 style='text-align: center;'>Factors affecting price</h3>", unsafe_allow_html=True)
     with st.expander("Price Estimation Over Different Production Years", expanded=False):
         plot_price_over_time(st.session_state.input_data_cpy, model_pred, preprocess_data)
-
-    # Expander for the second graph
     with st.expander("Average Estimated Price by Fuel Type", expanded=False):
         plot_category_comparison(st.session_state.input_data_cpy, model_pred, preprocess_data)
 
+# Plot function for category comparision
 def plot_category_comparison(input_data_category, model, preprocess_function):
     category_list = ['Jeep', 'Hatchback', 'Sedan', 'Microbus', 'Goods wagon', 'Universal', 'Coupe', 'Minivan', 'Cabriolet', 'Limousine', 'Pickup']
     average_prices = []
@@ -271,12 +265,10 @@ def plot_category_comparison(input_data_category, model, preprocess_function):
     for cat in category_list:
         temp_data = input_data_category.copy()
         temp_data['Category'] = cat
-
-        # You might need to adjust this part to handle multiple predictions and average them
         processed_data = preprocess_function(temp_data)
 
         prediction = model.predict(processed_data)
-        average_prices.append(np.mean(prediction))  # Assuming multiple predictions per fuel type
+        average_prices.append(np.mean(prediction))
 
     # Plotting
     fig, ax = plt.subplots(figsize=(13, 6))
@@ -286,24 +278,15 @@ def plot_category_comparison(input_data_category, model, preprocess_function):
     ax.set_ylabel('Average Estimated Price')
     st.pyplot(fig)
 
-
-
+# Plot function for price over time
 def plot_price_over_time(input_data_time, model, preprocess_function):
     years = np.arange(2000, 2024)
     prices = []
 
     for year in years:
         temp_data = input_data_time.copy()
-        temp_data['Prod_year'] = year  # Modify the production year in the original data format
-        print(temp_data)
-        # Process the data using your preprocessing function
+        temp_data['Prod_year'] = year
         processed_data = preprocess_function(temp_data)
-
-        # Ensure processed_data is in the correct format (e.g., dense format if it's sparse)
-        # If your preprocessing function returns a sparse matrix, convert it to dense
-        # if isinstance(processed_data, csr_matrix):
-        #     processed_data = processed_data.todense()
-
         prediction = model.predict(processed_data)
         prices.append(prediction[0])
 
@@ -314,8 +297,9 @@ def plot_price_over_time(input_data_time, model, preprocess_function):
     plt.xlabel('Production Year')
     plt.ylabel('Estimated Price')
     st.pyplot()
+
+
 # Streamlit UI components
-#st.markdown("<h1 style='text-align: center;'>Car Price Estimator</h1>", unsafe_allow_html=True)
 st.markdown("""
     <style>
     .heading-bar {
@@ -334,7 +318,7 @@ st.markdown("""
     </div>
     """, unsafe_allow_html=True)
 
-# Display the appropriate page
+# Displaying the appropriate page
 if st.session_state.report_page:
     report_page()
     if st.button('Back to Prediction'):
@@ -356,7 +340,6 @@ else:
         with next_col:
             st.button('Next', on_click=next_page)
 
-
     elif st.session_state.page_number == 3:
         page3()
         col1, col2, col3 = st.columns(3)
@@ -371,10 +354,6 @@ else:
             report_button = st.button('Generate Report', disabled=report_button_enabled)
 
         prediction_placeholder = st.empty()
-        # st.button('Back', on_click=prev_page)
-        # prediction_button = st.button('Predict')
-        # report_button = st.button('Report')
-        # prediction_placeholder = st.empty()
         if 'success_message' in st.session_state:
             st.success(st.session_state.success_message)
 
@@ -386,41 +365,17 @@ else:
                                                                                    'Gear_box_type', 'Drive_wheels', 'Doors',
                                                                                    'Wheel', 'Color', 'Airbags', 'With_Turbo'])
                 input_data_cpy = input_data.copy()
-                print("Before pred:", input_data)
-                processed_data = preprocess_data(input_data)  # Make sure this function is defined as in your Flask app
+                processed_data = preprocess_data(input_data)
                 # Make a prediction
                 prediction = model_pred.predict(processed_data)
                 # Display the prediction
                 time.sleep(4)
                 st.session_state.success_message = f'Estimated Price: ${prediction[0]}'
                 st.success(f'Estimated Price: ${prediction[0]}')
-                # plt.hist(input_data['Mileage'], bins=20, color='skyblue', alpha=0.7)
-                # plt.xlabel('Mileage')
-                # plt.ylabel('Frequency')
-                # plt.title('Mileage Distribution')
-                # st.pyplot()
-
-                # st.markdown('### Feature Importance in Price Prediction')
-                # plot_feature_importance(model_pred)
                 st.session_state.estimated_price = prediction[0]
                 st.session_state.input_data_cpy = input_data_cpy
                 st.session_state.prediction_made = True
                 st.experimental_rerun()
-                #st.markdown('### Price Estimation Over Different Production Years')
-                print("after pred:", input_data_cpy)
-                # plot_price_over_time(input_data_cpy, model_pred, preprocess_data)
-                # plot_category_comparison(input_data_cpy, model_pred, preprocess_data)
-                #prediction_placeholder.write(f'Estimated Price: ${prediction[0]}')
-                # st.markdown(
-                #     f"""
-                #     <div id="prediction_popup" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; border-radius: 10px; box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.2); z-index: 9999;">
-                #         <span onclick="document.getElementById('prediction_popup').style.display='none';" style="position: absolute; top: 5px; right: 10px; font-size: 20px; cursor: pointer;">&times;</span>
-                #         <p style="font-size: 18px; line-height: 1.6; margin-bottom: 15px;">Estimated Price: ${prediction[0]}</p>
-                #         <button onclick="document.getElementById('prediction_popup').style.display='none';" style="padding: 8px 15px; background-color: #4CAF50; color: white; border: none; border-radius: 5px; cursor: pointer;">Close</button>
-                #     </div>
-                #     """,
-                #     unsafe_allow_html=True
-                # )
         if report_button and st.session_state.prediction_made:
             st.session_state.report_page = True
             st.experimental_rerun()
